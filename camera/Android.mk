512063730f0c1a6b5b4f2c426ff0afb014f89196
aoleary <seanm187@gmail.com>
2020-04-08 13:10:07 +0000

    g4-common: camera: Resolve unknown target 'camera.msm8992' by using camera/Android.mk from Bullhead
    Taken from here: https://github.com/AICP/device_lge_bullhead/blob/p9.0/camera/Android.mk

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,22 +1,10 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-camera))
-MM_V4L2_DRIVER_LIST += msm8960
-MM_V4L2_DRIVER_LIST += msm8974
-MM_V4L2_DRIVER_LIST += msm8916
-MM_V4L2_DRIVER_LIST += msm8226
-MM_V4L2_DRIVER_LIST += msm8610
-MM_V4L2_DRIVER_LIST += apq8084
-MM_V4L2_DRIVER_LIST += mpq8092
-MM_V4L2_DRIVER_LIST += msm_bronze
-MM_V4L2_DRIVER_LIST += msm8916
-MM_V4L2_DRIVER_LIST += msm8994
-MM_V4L2_DRIVER_LIST += msm8084
-MM_V4L2_DRIVER_LIST += msm8992
-
-ifneq (,$(filter $(MM_V4L2_DRIVER_LIST),$(TARGET_BOARD_PLATFORM)))
-  ifneq ($(strip $(USE_CAMERA_STUB)),true)
-    ifneq ($(BUILD_TINY_ANDROID),true)
-      include $(call all-subdir-makefiles)
+# TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
+ifneq ($(TARGET_BOARD_AUTO),true)
+  ifneq ($(filter msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
+    ifneq ($(strip $(USE_CAMERA_STUB)),true)
+      ifneq ($(BUILD_TINY_ANDROID),true)
+        include $(call all-subdir-makefiles)
+      endif
     endif
   endif
-endif
 endif
